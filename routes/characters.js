bc0de198b6d2bcd14951fecd34a4b87e673752f4
Alexandre Klein <alexandre.klein@viacesi.fr>
2022-05-11 09:28:03 -0400

    add characters ID route

--- routes/characters.js
+++ routes/characters.js
@@ -3,4 +3,28 @@ const router = express.Router();
 const IsAuthenticated = require("../middlewares/IsAuthenticated");
 const Character = require("../models/Character");
 
+// get all the characters
+router.get("/characters", async (req, res) => {
+  try {
+    const comics = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(comics.data);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// get specific character
+router.get("/characterId/:characterId", async (req, res) => {
+  try {
+    const comic = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characterId/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(comic.data);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
 module.exports = router;
