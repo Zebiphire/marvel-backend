6eb22f74954e9a1412851d24700e8649ace4c579
Alexandre Klein <alexandre.klein@viacesi.fr>
2022-05-11 08:58:40 -0400

    fix get routes

--- routes/comics.js
+++ routes/comics.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const IsAuthenticated = require("../middlewares/IsAuthenticated");
-const Comic = require("../models/Comic");
 const axios = require("axios");
 
 // get all the comics
