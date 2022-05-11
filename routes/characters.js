b0e9e1e3ad55a813d94216092a00a87e8d6de0ee
Alexandre Klein <alexandre.klein@viacesi.fr>
2022-05-11 08:35:30 -0400

    add routes + models

---
+++ routes/characters.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const IsAuthenticated = require("../middlewares/IsAuthenticated");
+const Character = require("../models/Character");
---
+++ routes/comics.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const IsAuthenticated = require("../middlewares/IsAuthenticated");
+const Comic = require("../models/Comic");
+const axios = require("axios");
+
+// get all the comics
+router.post("/comics", async (req, res) => {
+  try {
+    const comics = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(comics.data);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// get specific comics
+router.get("/comics/:id", async (req, res) => {
+  try {
+    const comic = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.id}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(comic.data);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
